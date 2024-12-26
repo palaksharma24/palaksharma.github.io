909dc777cbc6b124ad7a17de53c314af6d879cb6
Palak Sharma <palak642@gmail.com>
2024-12-26 17:11:20 -0500

    Add portfolio

--- my-portfolio/src/app/app.routes.ts
+++ my-portfolio/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'portfolio', component: PortfolioComponent },
   { path: '**', redirectTo: '' }
 ];
--- my-portfolio/src/app/landing-page/landing-page.component.ts
+++ my-portfolio/src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  constructor(private router: Router) {}
 
+  navigateToPortfolio() {
+    this.router.navigate(['/portfolio']);
+  }
 }
